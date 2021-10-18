fb666f6c256a5e55d9bf2c5670a055cdc86a9948
udaysarkarud <udaysarkarud@gmail.com>
2021-10-18 23:31:50 +0600

    added basic components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,19 +5,50 @@ import useAuthContext from '../../hook/useAuthContext';
 const Header = () => {
     const { userProfile, handelGoogleSignOut } = useAuthContext()
     return (
-        <div>
-            <h2>This is Header Compo</h2>
-            {
-                userProfile?.email && <button onClick={handelGoogleSignOut}>Google SignOut</button>
-            }
+        <header>
+            <div class="container">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">
+                            <img src="images/logo.png" alt="" />
+                        </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#home-section">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#about-section">About</a>
+                                </li>
 
-            <br /><br /><br />
-            <Link to="/home">Home/ </Link>
-            <Link to="/details"> Details/</Link>
-            <Link to="/register"> Register/</Link>
-            <Link to="/login"> login</Link>
-            <Link to=""></Link>
-        </div>
+                                <li class="nav-item">
+                                    <a class="nav-link " href="#services-section">Services</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link " href="#reviews-section">Reviews</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link " href="#team-section">Our Doctors</a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link " href="#blog-section">Blog</a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link " href="#booking-section">Book Appointment</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
     );
 };
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,10 +4,20 @@ import useAuthContext from '../../hook/useAuthContext';
 const Home = () => {
     const { userProfile } = useAuthContext()
     return (
-        <div>
-            <h2>This is Home Compo</h2>
-            <p>{userProfile?.displayName}</p>
-        </div>
+        <section class="hero-banner" id="home-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 my-5">
+                        <div class="banner-text-wrap">
+                            <h1 class="text-white">We Provide Best Medical Services</h1>
+                            <p class="text-white">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy</p>
+                            <a href="#" class="btn btn-white">Book Appointment</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
